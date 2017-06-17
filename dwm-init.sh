be1aec238c1c275231c7e9915ea20d3df72ae756
Rafael <rafaelleru95103@gmail.com>
2017-06-17 20:15:01 +0200

    update

--- dwm-init.sh
+++ dwm-init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 exec nm-applet &
+exec blueman-applet &
 exec slstatus &
-exec emacsclient -c -a "" -e "(my-org-agenda-list)" &
-exec spotify &
-exec enpass &
 exec feh --bg-scale /home/rafa/Pictures/mountain.jpg &
+exec wmname LG3D
 # exec dwm
 
 while true; do
