2952652db0c229c401399b67f40027c41b4e5b9a
Rafa leyva <rafaelleru95103@gmail.com>
2020-04-08 08:23:08 +0200

    Add systray patch  changes to my custom dwm config

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -3,6 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Code Pro:size=10", "SauceCodePro Nerd Font:style=Regular:size=13", "FontAwesome:pixelsize=12", "Font Awesome 5 Free:pixelsize=12", "Jam\-icons:style=Regular:pixelsize=12" };
@@ -62,7 +66,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *clipboardcmp[]  = { "clipmenu", NULL };
-static const char *passmenucmd[]  = { "/usr/share/doc/pass/examples/dmenu/passmenu", "-fn", "Source Code Pro" "-l", "10" };
+static const char *passmenucmd[]  = { "/usr/share/doc/pass/examples/dmenu/passmenu", "-fn", "Source Code Pro" "-l", "10", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
