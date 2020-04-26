f3cded79c9a2977a90794fb8b000edbb0964119c
Rafa leyva <rafaelleru95103@gmail.com>
2020-04-26 13:47:59 +0200

    Add wmname command to xinitrc script

--- bin/scripts/run_dwm.sh
+++ bin/scripts/run_dwm.sh
@@ -1,12 +1,13 @@
 source $HOME/.profile
 $HOME/bin/earthwall/earthwall.sh&
-pulseaudio --daemonize
+start-pulseaudio-x11
 clipmenud&
 compton&
 thunderbird&
-#slstatus&
 clipmenud&
 dunst&
+wmname LG3D
+blueman-applet&
 $HOME/dotfiles/bin/scripts/status.sh&
 
 exec dbus-launch `dwm 2> /tmp/dwm.log`
