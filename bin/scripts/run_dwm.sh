936ae8c066b68912c1a93037f78f963a8d9cbb0b
Rafa leyva <rafaelleru95103@gmail.com>
2020-12-01 12:31:00 +0100

    Use redshift to adjust screen temperature

--- bin/scripts/run_dwm.sh
+++ bin/scripts/run_dwm.sh
@@ -1,15 +1,12 @@
 source $HOME/.profile
-# $HOME/bin/earthwall/earthwall.sh&
-feh --bg-scale /home/rl/Pictures/wallpaper.jpg
+feh --bg-scale $HOME/Pictures/wallpaper.jpg
 start-pulseaudio-x11
-clipmenud&
-compton&
-thunderbird&
+#compton&
 clipmenud&
 dunst&
 wmname LG3D
 blueman-applet&
 $HOME/dotfiles/bin/scripts/status.sh&
-xflux -l 37.2 -g -3.61
+redshift -l 37.2:-3.61 &
 
 exec dbus-launch `dwm 2> /tmp/dwm.log`
