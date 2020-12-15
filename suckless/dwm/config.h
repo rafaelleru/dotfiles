545f8e784c6215cc45715d9f5ed08a65f553b868
Rafa leyva <rafaelleru95103@gmail.com>
2020-12-15 12:51:20 +0100

    Use kitty as default terminal in dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *passmenucmd[]  = { "passmenu", NULL };
 static const char *clipmenucmd[]  = { "clipmenu", NULL };
 static const char *soundupcmd[]  = { "amixer", "-q", "sset", "Master", "5%+", NULL };
