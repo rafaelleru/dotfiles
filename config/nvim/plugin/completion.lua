0f8b3c7ae1948047872cad4fa730e411c90fa77c
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2024-06-14 13:14:08 +0200

    nvim fix completion not working completly fine

--- config/nvim/plugin/completion.lua
+++ config/nvim/plugin/completion.lua
@@ -44,28 +44,40 @@ cmp.setup({
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    ['<CR>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+              if luasnip.expandable() then
+                  luasnip.expand()
+              else
+                  cmp.confirm({
+                      select = true,
+                  })
+              end
+          else
+              fallback()
+          end
+      end),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.locally_jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    }),
 })
 
 -- Configuración de cmdline
