3ffad32b19b22c23a4d97e4ff3855a0ad3d01756
Rafael Leyva <rafaelleru95103@gmail.com>
2024-12-13 21:24:43 +0100

    (nvim) Added some initial treesiter config

---
+++ config/nvim/lua/plugins/treesiter.lua
@@ -0,0 +1,19 @@
+return {
+		{
+				"nvim-treesitter/nvim-treesitter",
+				config = function()
+						require("nvim-treesitter").setup({
+								ensure_installed = {"python", "lua"},
+								highlight = {
+										disable = function(lang, buf)
+												local max_filesize = 100 * 1024 -- 100 KB
+												local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+												if ok and stats and stats.size > max_filesize then
+														return true
+												end
+										end
+								}
+						})
+				end
+		}
+}
