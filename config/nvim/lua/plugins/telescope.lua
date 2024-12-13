def08b4f25473f1d91a92e909738a39bd547f3e8
Rafael Leyva <rafaelleru95103@gmail.com>
2024-12-13 21:25:04 +0100

    (nvim) Add telescope initial config

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+		{
+				'nvim-telescope/telescope.nvim', tag = '0.1.8',
+				dependencies = { 'nvim-lua/plenary.nvim' },
+				config = function()
+						require("telescope").setup({})
+				end
+		}
+}
