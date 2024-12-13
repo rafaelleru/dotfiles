cdd4b60d3d18c4150e6fbe083da93a6e850d634a
Rafael Leyva <rafaelleru95103@gmail.com>
2024-12-13 21:37:18 +0100

    (nvim) Added some mappings

---
+++ config/nvim/plugin/mappings.lua
@@ -0,0 +1,7 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<c-/>', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<c-b>', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>w', ":w<CR>")
+vim.keymap.set('n', '<leader>x', ":xCR>")
