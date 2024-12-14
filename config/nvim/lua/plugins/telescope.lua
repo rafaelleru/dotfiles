cac3950ab4deda93c48316d05649f2ab23429e26
Rafael Leyva <rafaelleru95103@gmail.com>
2024-12-14 18:30:30 +0100

    (nvim) Set ivy theme for file pickers telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,16 @@ return {
 				'nvim-telescope/telescope.nvim', tag = '0.1.8',
 				dependencies = { 'nvim-lua/plenary.nvim' },
 				config = function()
-						require("telescope").setup({})
+						require("telescope").setup({
+								pickers = {
+										find_files = {
+												theme = "ivy"
+										},
+										git_files = {
+												theme = "ivy"
+										}
+								}
+						})
 				end
 		}
 }
