645b1d73b888811c76596c93346824ab2fba81c5
Rafael Leyva <rafaelleru95103@gmail.com>
2024-12-14 18:29:09 +0100

    (nvim) Add lazydev plugin for lua development

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -5,5 +5,11 @@ return {
 						require("lspconfig").lua_ls.setup {}
 						require("lspconfig").pylsp.setup {}
 				end
+		},
+		{
+				'folke/lazydev.nvim',
+				config = function ()
+					require('lazydev').setup()
+				end
 		}
 }
