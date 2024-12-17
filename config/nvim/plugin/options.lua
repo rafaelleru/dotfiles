4d57a3f6a3c37e55748e77310001c81015fde38b
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2024-12-17 15:07:32 +0100

    (nvim) setup default options

--- config/nvim/plugin/options.lua
+++ config/nvim/plugin/options.lua
@@ -1,3 +1,36 @@
-vim.cmd.colorscheme = "tokyonight-night"
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
+local set = vim.opt
+set.relativenumber = true
+set.wildmenu = true
+set.hidden = true
+set.hlsearch = false
+set.showmatch = true
+set.encoding = "utf-8"
+set.clipboard = "unnamedplus"
+set.termguicolors = true
+set.undofile = true
+set.undodir = vim.fn.expand("$HOME/.local/vim_undo_dir")
+set.path:append("**")
+set.wildignore:append("*.pyc,*~")
+set.swapfile = false
+set.list = true
+set.mouse = "nv"
+set.cursorline = true
+set.foldlevelstart = 1
+set.scrolloff = 10
+set.compatible = false
+set.tabstop = 4
+set.exrc = true
+set.splitright = true
+set.splitbelow = true
+
+require("catppuccin").setup({
+    flavor = 'macchiato'
+})
+vim.cmd("syntax enable")
+vim.cmd("colorscheme catppuccin")
+
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+vim.cmd("hi LineNr guibg=NONE ctermbg=NONE")
+vim.cmd("hi SignColumn guibg=NONE ctermbg=NONE")
+vim.cmd("hi EndOfBuffer guibg=NONE ctermbg=NONE")
+vim.cmd("hi Comment gui=italic cterm=italic")
