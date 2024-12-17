e677f3e2d95b67351b53d8309bdf5de997993104
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2024-12-17 15:05:29 +0100

    (nvim) auto formated telescope config

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,29 @@
 return {
-		{
-				'nvim-telescope/telescope.nvim', tag = '0.1.8',
-				dependencies = { 'nvim-lua/plenary.nvim' },
-				config = function()
-						require("telescope").setup({
-								pickers = {
-										find_files = {
-												theme = "ivy"
-										},
-										git_files = {
-												theme = "ivy"
-										}
-								}
-						})
-				end
-		}
+	{ 'junegunn/fzf', run = function() vim.fn['fzf#install']() end },
+	{ 'junegunn/fzf.vim' },
+	{
+		'nvim-telescope/telescope-fzf-native.nvim',
+		run = 'make'
+	},
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope-live-grep-args.nvim'
+		},
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						theme = "ivy"
+					},
+					git_files = {
+						theme = "ivy"
+					}
+				}
+			})
+			require('telescope').load_extension('fzf')
+			require("telescope").load_extension("live_grep_args")
+		end
+	}
 }
