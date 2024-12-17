39b6ea163e2fbdb412108722d9fdeed60afede2e
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2024-12-17 15:02:48 +0100

    (nvim) Use pass to get openAI api key

--- config/nvim/lua/plugins/chatgpt.lua
+++ config/nvim/lua/plugins/chatgpt.lua
@@ -8,9 +8,8 @@ return {
 						"nvim-telescope/telescope.nvim"
 				},
 				config = function ()
-						local home = vim.fn.expand("$HOME")
 						require("chatgpt").setup({
-								api_key_cmd = "gpg --decrypt " .. home .. "/secret.txt.gpg"
+								api_key_cmd = "pass chatopenai"
 						})
 				end
 		}
