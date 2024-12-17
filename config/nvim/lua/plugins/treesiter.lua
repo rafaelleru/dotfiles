a6908829031c8fc193f9b29a34148bdeeb3269e1
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2024-12-17 15:06:17 +0100

    (nvim) enable incremental selection for treesiter in visual mode

--- config/nvim/lua/plugins/treesiter.lua
+++ config/nvim/lua/plugins/treesiter.lua
@@ -1,19 +1,40 @@
 return {
-		{
-				"nvim-treesitter/nvim-treesitter",
-				config = function()
-						require("nvim-treesitter").setup({
-								ensure_installed = {"python", "lua"},
-								highlight = {
-										disable = function(lang, buf)
-												local max_filesize = 100 * 1024 -- 100 KB
-												local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-												if ok and stats and stats.size > max_filesize then
-														return true
-												end
-										end
-								}
-						})
-				end
-		}
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			'andymass/vim-matchup'
+		},
+		config = function()
+			require("nvim-treesitter").setup({
+				ensure_installed = { "python", "lua", "terraform", "php", "yaml" },
+				auto_install = true,
+				highlight = {
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end
+				},
+				matchup = {
+					enable = true
+				}
+			})
+			require('nvim-treesitter.configs').setup({
+				highlight = {
+					enable = true
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = " vv",
+						node_incremental = "nu",
+						--scope_incremental = "U",
+						node_decremental = "nd",
+					},
+				}
+			})
+		end
+	}
 }
