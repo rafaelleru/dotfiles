ea829dd7a04f0c12645a34032c91b5cee1a76801
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2024-12-17 15:04:26 +0100

    (nvim) configure pylsp

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,26 @@
 return {
-		{
-				"neovim/nvim-lspconfig",
-				config = function()
-						require("lspconfig").lua_ls.setup {}
-						require("lspconfig").pylsp.setup {}
-				end
-		},
-		{
-				'folke/lazydev.nvim',
-				config = function ()
-					require('lazydev').setup()
-				end
-		}
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require('lspconfig')
+			lspconfig.lua_ls.setup {}
+			lspconfig.pylsp.setup {
+				settings = {
+					pylsp = {
+						plugins = {
+							pycodestyle = {
+								indentSize = 4,
+								maxLineLength = 120,
+								ignore = {'W191'}
+							},
+							flake8 = {
+								indentSize = 4
+							}
+						}
+					}
+				}
+			}
+			require('lspconfig').terraformls.setup{}
+		end
+	}
 }
