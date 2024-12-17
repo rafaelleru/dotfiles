37132df7c7cc6b298d9c1fbf2409996a0b8538dc
Rafa Leyva <rafael.leyva@constellaintelligence.com>
2024-12-17 15:33:05 +0100

    (nvim) removed plugin files from rebase

--- config/nvim/plugin/mason.lua
+++
@@ -1,6 +0,0 @@
-require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = { "pylsp", "rust_analyzer", "clangd", "vimls", "solargraph" },
-    automatic_installation = true,
-    log_level = vim.log.levels.DEBUG, 
-})
